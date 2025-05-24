ed60a77581d9639eceaf9c8efb6399040fd8fe94
33b3ziz <moabdelaziz3225@gmail.com>
2025-05-24 12:54:07 +0300

    -

--- buffers/different-allocs.js
+++ buffers/different-allocs.js
@@ -7,12 +7,15 @@ const unsafeBuffer = Buffer.allocUnsafe(10000); // faster but not zeroed
 
 const buff = Buffer.allocUnsafeSlow(2); // It won't make use of the pool
 
-for (let i = 0; i < unsafeBuffer.length; i++) {
-  if (unsafeBuffer[i] !== 0) {
-    console.log(
-      `element at ${i} is not zeroed, it has value: ${unsafeBuffer[i].toString(
-        2
-      )}`
-    );
-  }
-}
+// Buffer.from();
+// Buffer.concat(); // both methods use allocUnsafe behind the scenes
+
+// for (let i = 0; i < unsafeBuffer.length; i++) {
+//   if (unsafeBuffer[i] !== 0) {
+//     console.log(
+//       `element at ${i} is not zeroed, it has value: ${unsafeBuffer[i].toString(
+//         2
+//       )}`
+//     );
+//   }
+// }
